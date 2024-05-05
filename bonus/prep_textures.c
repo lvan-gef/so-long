3f0342847663489252afd06354936cc3d953d6fa
luuk <lvan-gef@student.codam.nl>
2024-05-05 22:36:15 +0200

    Fixed posible NullDereference/leak

--- bonus/prep_textures.c
+++ bonus/prep_textures.c
@@ -6,13 +6,13 @@
 /*   By: lvan-gef <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/16 16:55:51 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2023/04/16 16:55:58 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/05/05 22:35:57 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static	char	*real_path(t_game *game, char *suffix, char *filename)
+static	char	*real_path(char *suffix, char *filename)
 {
 	char	*fullname;
 
@@ -20,7 +20,6 @@ static	char	*real_path(t_game *game, char *suffix, char *filename)
 	if (!fullname)
 	{
 		free(suffix);
-		init_error(game, "Faild to make a path");
 		return (NULL);
 	}
 	return (fullname);
@@ -28,23 +27,41 @@ static	char	*real_path(t_game *game, char *suffix, char *filename)
 
 static	void	set_paths(t_game *game, char *suffix)
 {
-	game->src_img[0] = real_path(game, suffix, "/ground_05.png");
-	game->src_img[1] = real_path(game, suffix, "/crate_01.png");
-	game->src_img[2] = real_path(game, suffix, "/environment_12.png");
-	game->src_img[3] = real_path(game, suffix, "/environment_03.png");
-	game->src_img[4] = real_path(game, suffix, "/player_a_idle.png");
-	game->src_img[5] = real_path(game, suffix, "/player_a.png");
-	game->src_img[6] = real_path(game, suffix, "/player_a_midle.png");
-	game->src_img[7] = real_path(game, suffix, "/player_w_idle.png");
-	game->src_img[8] = real_path(game, suffix, "/player_w.png");
-	game->src_img[9] = real_path(game, suffix, "/player_w_midle.png");
-	game->src_img[10] = real_path(game, suffix, "/player_s_idle.png");
-	game->src_img[11] = real_path(game, suffix, "/player_s.png");
-	game->src_img[12] = real_path(game, suffix, "/player_s_midle.png");
-	game->src_img[13] = real_path(game, suffix, "/player_d_idle.png");
-	game->src_img[14] = real_path(game, suffix, "/player_d.png");
-	game->src_img[15] = real_path(game, suffix, "/player_d_midle.png");
-	game->src_img[16] = real_path(game, suffix, "/enemy.png");
+	game->src_img[0] = real_path(suffix, "/ground_05.png");
+	game->src_img[1] = real_path(suffix, "/crate_01.png");
+	game->src_img[2] = real_path(suffix, "/environment_12.png");
+	game->src_img[3] = real_path(suffix, "/environment_03.png");
+	game->src_img[4] = real_path(suffix, "/player_a_idle.png");
+	game->src_img[5] = real_path(suffix, "/player_a.png");
+	game->src_img[6] = real_path(suffix, "/player_a_midle.png");
+	game->src_img[7] = real_path(suffix, "/player_w_idle.png");
+	game->src_img[8] = real_path(suffix, "/player_w.png");
+	game->src_img[9] = real_path(suffix, "/player_w_midle.png");
+	game->src_img[10] = real_path(suffix, "/player_s_idle.png");
+	game->src_img[11] = real_path(suffix, "/player_s.png");
+	game->src_img[12] = real_path(suffix, "/player_s_midle.png");
+	game->src_img[13] = real_path(suffix, "/player_d_idle.png");
+	game->src_img[14] = real_path(suffix, "/player_d.png");
+	game->src_img[15] = real_path(suffix, "/player_d_midle.png");
+	game->src_img[16] = real_path(suffix, "/enemy.png");
+}
+
+static	void	check_paths(t_game *game)
+{
+	size_t	max_size;
+	size_t	counter;
+
+	max_size = 17;
+	counter = 0;
+	while (counter < max_size)
+	{
+		if (game->src_img[counter] == NULL)
+		{
+			path_error(game, NULL, "No valid path");
+			return ;
+		}
+		counter++;
+	}
 }
 
 void	imgs_path(t_game *game)
@@ -73,4 +90,5 @@ void	imgs_path(t_game *game)
 	}
 	set_paths(game, suffix);
 	free(suffix);
+	check_paths(game);
 }
